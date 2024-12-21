1ebd6948024650746f14401d899f0c60876f299d
Josephjj224 <eldverry52@gmail.com>
2024-12-21 14:32:38 -0600

    Remove deleted files

--- file_web_app/src/components/Main.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Container } from "@chakra-ui/react";
-const Main = ({children, ...props}) => {
-    return <Container as="main" flexDir="column" justifyContent="center" centerContent {...props}>
-        {children}
-    </Container>;
-};
-
-export default Main;
--- src/components/ErrorMessage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Text } from "@chakra-ui/react";
-
-const ErrorMessage = ({ error }) =>
-  error ? <Text color="red.500">{error}</Text> : null;
-
-export default ErrorMessage;
